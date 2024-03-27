f02f5ab06ff54134028e195d2dc617f86f5f0425
Nitwel <mail@nitwel.de>
2024-03-27 02:15:51 +0100

    add phantom camera and test out race path

--- Player.gd
+++ Player.gd
@@ -4,8 +4,7 @@ extends Node3D
 @export var kick_up_boost = 15
 
 @onready var body: RigidBody3D = $Body
-@onready var ball: RigidBody3D = $"/root/Main/Ball"
-@onready var camera: Camera3D = $Node3D/Camera3D
+@onready var camera: PhantomCamera3D = $PhantomCamera3D
 
 func _enter_tree():
     set_multiplayer_authority(str(name).to_int())
@@ -13,20 +12,5 @@ func _enter_tree():
 func _ready():
     if !is_multiplayer_authority(): return
 
-    camera.current = true
-
 func _physics_process(delta):
     if !is_multiplayer_authority(): return
-
-    if Input.is_action_just_pressed("kick"):
-        var distance = (ball.global_position - body.global_position)
-
-        if distance.length() > 2.2:
-            return
-
-        var direction = distance.normalized() * kick_strength
-        direction.y = kick_up_boost
-
-        body.apply_impulse( - direction * 0.4)
-
-        ball.apply_impulse(direction)
--- body.gd
+++ body.gd
@@ -3,15 +3,35 @@ extends RigidBody3D
 @export var speed = 0.4
 @export var max_speed = 10
 
-func _integrate_forces(state):
-    if !get_parent().is_multiplayer_authority(): return
+@onready var camera = $"../PhantomCamera3D"
+@onready var look_target = $"../CameraLookTarget"
 
-    var input: Vector2 = Input.get_vector("left", "right", "up", "down", 0.0)
+func _ready():
+	var existing_path = get_node("/root/Main/Map/Path3D")
 
-    if input.length() < 0.1:
-        return
+	var new_path = Path3D.new()
+	new_path.curve = existing_path.curve.duplicate()
+	new_path.position = Vector3(0, 4, 0)
+	existing_path.get_parent().add_child(new_path)
+	camera.set_follow_path(new_path)
 
-    input = input.normalized() * speed
+func _integrate_forces(state: PhysicsDirectBodyState3D):
+	if !get_parent().is_multiplayer_authority(): return
 
-    state.linear_velocity += Vector3(input.x, 0, input.y)
-    state.linear_velocity = state.linear_velocity.clamp(Vector3( - max_speed, -max_speed, -max_speed), Vector3(max_speed, max_speed, max_speed))
+	var path: Path3D = get_node("/root/Main/Map/Path3D")
+	var target_offset = path.curve.get_closest_offset(path.to_local(global_position))
+	target_offset = fmod(target_offset + 4.0, path.curve.get_baked_length())
+
+	var target = path.curve.sample_baked(target_offset)
+
+	look_target.global_position = path.to_global(target) + Vector3(0, 1.5, 0)
+
+	var input: Vector2 = Input.get_vector("left", "right", "up", "down", 0.0)
+
+	if input.length() < 0.1:
+		return
+
+	input = input.rotated( - camera.global_rotation.y).normalized() * speed
+
+	state.linear_velocity += Vector3(input.x, 0, input.y)
+	state.linear_velocity = state.linear_velocity.clamp(Vector3( - max_speed, -max_speed, -max_speed), Vector3(max_speed, max_speed, max_speed))
